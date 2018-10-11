800a0c0431897acf26a4cbe00e284cdb509dd8ff
Connor Christensen <chriconn@oregonstate.edu>
2018-10-11 10:22:37 -0700

    feat: Get a simple OpenGL box and x,y,z sliders
    This is the starting layout for glman and will be improved from here

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,17 +1,218 @@
-from fbs_runtime.application_context import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
+#!/usr/bin/env python
 
 import sys
+import math
 
-class AppContext(ApplicationContext):           # 1. Subclass ApplicationContext
-    def run(self):                              # 2. Implement run()
-        window = QMainWindow()
-        window.setWindowTitle('Hello World!')
-        window.resize(250, 150)
-        window.show()
-        return self.app.exec_()                 # 3. End run() with this line
+from PyQt5.QtCore import pyqtSignal, QPoint, QSize, Qt
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QOpenGLWidget, QSlider,
+                             QWidget)
+
+import OpenGL.GL as gl
+
+
+class Window(QWidget):
+
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.glWidget = GLWidget()
+
+        # create each slider
+        self.xSlider = self.createSlider()
+        self.ySlider = self.createSlider()
+        self.zSlider = self.createSlider()
+
+        # connect a value changed listener to the sliders
+        self.xSlider.valueChanged.connect(self.glWidget.setXRotation)
+        self.glWidget.xRotationChanged.connect(self.xSlider.setValue)
+        self.ySlider.valueChanged.connect(self.glWidget.setYRotation)
+        self.glWidget.yRotationChanged.connect(self.ySlider.setValue)
+        self.zSlider.valueChanged.connect(self.glWidget.setZRotation)
+        self.glWidget.zRotationChanged.connect(self.zSlider.setValue)
+
+        # add the widgets to the menu
+        mainLayout = QHBoxLayout()
+        mainLayout.addWidget(self.glWidget)
+        mainLayout.addWidget(self.xSlider)
+        mainLayout.addWidget(self.ySlider)
+        mainLayout.addWidget(self.zSlider)
+        self.setLayout(mainLayout)
+
+        # set the starting values of the slider
+        self.xSlider.setValue(500)
+        self.ySlider.setValue(2000)
+        self.zSlider.setValue(0)
+
+        self.setWindowTitle("glman")
+
+    def createSlider(self):
+        slider = QSlider(Qt.Vertical)
+
+        slider.setRange(0, 360 * 16)
+        slider.setSingleStep(16)
+        slider.setPageStep(15 * 16)
+        slider.setTickInterval(15 * 16)
+        slider.setTickPosition(QSlider.TicksRight)
+
+        return slider
+
+
+class GLWidget(QOpenGLWidget):
+    xRotationChanged = pyqtSignal(int)
+    yRotationChanged = pyqtSignal(int)
+    zRotationChanged = pyqtSignal(int)
+
+    def __init__(self, parent=None):
+        super(GLWidget, self).__init__(parent)
+
+        self.object = 0
+        self.xRot = 0
+        self.yRot = 0
+        self.zRot = 0
+
+        self.lastPos = QPoint()
+
+        self.backgroundColor = QColor.fromCmykF(0.0, 0.0, 0.0, 0.0)
+
+    def getOpenglInfo(self):
+        info = """
+            Vendor: {0}
+            Renderer: {1}
+            OpenGL Version: {2}
+            Shader Version: {3}
+        """.format(
+            gl.glGetString(gl.GL_VENDOR),
+            gl.glGetString(gl.GL_RENDERER),
+            gl.glGetString(gl.GL_VERSION),
+            gl.glGetString(gl.GL_SHADING_LANGUAGE_VERSION)
+        )
+
+        return info
+
+    def minimumSizeHint(self):
+        return QSize(50, 50)
+
+    def sizeHint(self):
+        return QSize(400, 400)
+
+    def setXRotation(self, angle):
+        angle = self.normalizeAngle(angle)
+        if angle != self.xRot:
+            self.xRot = angle
+            self.xRotationChanged.emit(angle)
+            self.update()
+
+    def setYRotation(self, angle):
+        angle = self.normalizeAngle(angle)
+        if angle != self.yRot:
+            self.yRot = angle
+            self.yRotationChanged.emit(angle)
+            self.update()
+
+    def setZRotation(self, angle):
+        angle = self.normalizeAngle(angle)
+        if angle != self.zRot:
+            self.zRot = angle
+            self.zRotationChanged.emit(angle)
+            self.update()
+
+    def initializeGL(self):
+        print(self.getOpenglInfo())
+
+        self.setClearColor(self.backgroundColor.darker())
+        self.object = self.MjbWireBox(.5, .5, -.5)
+        gl.glShadeModel(gl.GL_SMOOTH)
+        gl.glEnable(gl.GL_DEPTH_TEST)
+        gl.glEnable(gl.GL_CULL_FACE)
+
+    def paintGL(self):
+        gl.glClear(
+            gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        gl.glLoadIdentity()
+        gl.glTranslated(0.0, 0.0, -10.0)
+        gl.glRotated(self.xRot / 16.0, 1.0, 0.0, 0.0)
+        gl.glRotated(self.yRot / 16.0, 0.0, 1.0, 0.0)
+        gl.glRotated(self.zRot / 16.0, 0.0, 0.0, 1.0)
+        gl.glCallList(self.object)
+
+    def resizeGL(self, width, height):
+        side = min(width, height)
+        if side < 0:
+            return
+
+        gl.glViewport((width - side) // 2, (height - side) // 2, side, side)
+
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glLoadIdentity()
+        gl.glOrtho(-0.5, +0.5, +0.5, -0.5, 4.0, 15.0)
+        gl.glMatrixMode(gl.GL_MODELVIEW)
+
+    def mousePressEvent(self, event):
+        self.lastPos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        dx = event.x() - self.lastPos.x()
+        dy = event.y() - self.lastPos.y()
+
+        if event.buttons() & Qt.LeftButton:
+            self.setXRotation(self.xRot + 8 * dy)
+            self.setYRotation(self.yRot + 8 * dx)
+        elif event.buttons() & Qt.RightButton:
+            self.setXRotation(self.xRot + 8 * dy)
+            self.setZRotation(self.zRot + 8 * dx)
+
+        self.lastPos = event.pos()
+
+    def MjbWireBox(self, dx, dy, dz):
+        genList = gl.glGenLists(1)
+        gl.glNewList(genList, gl.GL_COMPILE)
+
+        dx /= 2.0
+        dy /= 2.0
+        dz /= 2.0
+
+        gl.glBegin(gl.GL_LINE_STRIP)
+        gl.glVertex3f(-dx, -dy, -dz)
+        gl.glVertex3f(dx, -dy, -dz)
+        gl.glVertex3f(dx, dy, -dz)
+        gl.glVertex3f(-dx, dy, -dz)
+        gl.glVertex3f(-dx, -dy, -dz)
+        gl.glVertex3f(-dx, -dy, dz)
+        gl.glVertex3f(dx, -dy, dz)
+        gl.glVertex3f(dx, dy, dz)
+        gl.glVertex3f(-dx, dy, dz)
+        gl.glVertex3f(-dx, -dy, dz)
+        gl.glEnd()
+
+        gl.glBegin(gl.GL_LINES)
+        gl.glVertex3f(dx, -dy, -dz)
+        gl.glVertex3f(dx, -dy, dz)
+        gl.glVertex3f(dx, dy, -dz)
+        gl.glVertex3f(dx, dy, dz)
+        gl.glVertex3f(-dx, dy, -dz)
+        gl.glVertex3f(-dx, dy, dz)
+        gl.glEnd()
+        gl.glEndList()
+
+        return genList
+
+    def normalizeAngle(self, angle):
+        while angle < 0:
+            angle += 360 * 16
+        while angle > 360 * 16:
+            angle -= 360 * 16
+        return angle
+
+    def setClearColor(self, c):
+        gl.glClearColor(c.redF(), c.greenF(), c.blueF(), c.alphaF())
+
+    def setColor(self, c):
+        gl.glColor4f(c.redF(), c.greenF(), c.blueF(), c.alphaF())
+
 
 if __name__ == '__main__':
-    appctxt = AppContext()                      # 4. Instantiate the subclass
-    exit_code = appctxt.run()                   # 5. Invoke run()
-    sys.exit(exit_code)
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
